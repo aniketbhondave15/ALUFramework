593a3fc26974d6cdbb62a3b47be64484fc1579a2
aniketbhondave15 <iamrefrain@gmail.com>
2022-07-19 15:04:10 +0530

    Add files via upload

---
+++ ALUFrameworkSecond.xcframework/ios-arm64_x86_64-simulator/ALUFrameworkSecond.framework/Headers/ALUFrameworkSecond.h
@@ -0,0 +1,18 @@
+//
+//  ALUFrameworkSecond.h
+//  ALUFrameworkSecond
+//
+//  Created by MobileDev on 19/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ALUFrameworkSecond.
+FOUNDATION_EXPORT double ALUFrameworkSecondVersionNumber;
+
+//! Project version string for ALUFrameworkSecond.
+FOUNDATION_EXPORT const unsigned char ALUFrameworkSecondVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALUFrameworkSecond/PublicHeader.h>
+
+
